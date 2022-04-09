b336465725de599f5c8cf85cf59590201b0f1dc0
Davuluri Hemanth Chowdary <hemanth.davuluri@minutestoseconds.com>
2022-04-09 13:35:46 +0530

    v1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+/* eslint-disable react/no-unescaped-entities */
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import FacebookLogo from "../assets/icons8-facebook.svg";
+import TwitterLogo from "../assets/icons8-twitter.svg";
+import InstagramLogo from "../assets/icons8-instagram.svg";
 
 export default function Home() {
   return (
@@ -11,7 +15,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>.....loading⌛</h1>
+      <div className={styles.header}>
+        <h1>DH.</h1>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.ninety}>
+          <h3>I'm Hemanth Davuluri.</h3>
+          <h1>A Full-Stack</h1>
+          <h1>Application Developer.</h1>
+        </div>
+        <div className={styles.ten}>
+          <Image
+            className={styles.logos}
+            src={FacebookLogo}
+            width={25}
+            height={25}
+            alt="facebook-logo"
+          />
+          <Image
+            className={styles.logos}
+            src={InstagramLogo}
+            width={25}
+            height={25}
+            alt="instagram-logo"
+          />
+          <Image
+            className={styles.logos}
+            src={TwitterLogo}
+            width={25}
+            height={25}
+            alt="twitter-logo"
+          />
+        </div>
+      </div>
     </div>
   );
 }
